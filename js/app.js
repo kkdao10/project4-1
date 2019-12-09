9a38d33620f1e747c96d523349239d8fc82aefc1
Kelvin Dao <kdao5@horizon.csueastbay.edu>
2019-12-09 10:37:44 -0800

    big update
    added more onscroll pop up animations

--- js/app.js
+++ js/app.js
@@ -4,73 +4,68 @@ $(document).foundation()
 
 
 
-
-
-
 //https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_shrink_header_scroll
    //When the user scrolls down 50px from the top of the
 
 
   //document, resize the header's font size
-  window.onscroll = function() {scrollFunction()};
-  function scrollFunction()
+  window.onscroll = function()
+   {scrollFunction()};
+
+          function scrollFunction()
   {
 
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50)
+      if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50)
             {
               document.getElementById("header").style.fontSize = "50px";
             }
-    else
-              document.getElementById("header").style.fontSize = "100px";
-            }
+              else
+              {
+                document.getElementById("header").style.fontSize = "100px";
+              }
 
 
 
 
+// these js code for the rotating square demo https://jsbin.com/yoqevajawo/edit?html,css,js,output
+  var counter = document.getElementById("counter");
 
 
-      
-
-
-
-
-
-
-
-
-
-
-
-
-  //var counter = document.getElementById("counter");
-
-//  window.onscroll = function()
-  //  {
-  //    console.log(window.pageYOffset);
-  //    var distance = window.pageYOffset;
-  //counter.innerHTML = distance;
-  // counter.style.top = distance *0.98 +100 + "px";
+      console.log(window.pageYOffset);
+      var distance = window.pageYOffset;
+  counter.innerHTML = distance;
+   counter.style.top = distance *0.98 +450 ;
    /* counter.style.left = distance *0.2  + "px"; */
-  //  counter.style.transform="rotate("+distance*.2+"deg)"
+    counter.style.transform="rotate("+distance*.2+"deg)"
 
-  //  if (distance > 1000)
-  //    {
-    //    counter.style.color="black";
-  //    }
-  //  else
-  //    {
-    //    counter.style.color="white";
-  //    }
+    if (distance > 1000)
+      {
+        counter.style.color="black";
+        counter.style.animationPlayState="running";
+      }
+    else
+      {
+        counter.style.color="white";
+      }
 
-  //  if (distance > 2000)
-  //    {
-  //      counter.style.backgroundColor = "yellow";
-  //    }
-  //  else
-  //    {
-  //      counter.style.backgroundColor = "red";
-  //    }
-  //  }
+    if (distance > 2000)
+      {
+       counter.style.backgroundColor = "lightblue";
+     }
+    else
+      {
+        counter.style.backgroundColor = "grey";
+      }
+    }
+
+  //  var counter = document.getElementById("slideinleft");
+  //  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50)
+    //    {
+    //      document.getElementById("slideinleft") && document.getElementById("slideinright");
+    //    }
+
+
+
 
 
 //var $moving = $('.moving');
